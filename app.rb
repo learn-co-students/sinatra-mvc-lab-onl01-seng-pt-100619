6eba973c400ead8a3e20902b062418270c094de2
allison cortez <allizetorc@gmail.com>
2020-01-23 22:27:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+  
+  post '/' do
+    # text_from_user = params[:user_text]
+    
+    # @analyzed_text = TextAnalyzer.new(text_from_user)
+
+
+#shortened version of lines 10 and 12
+@analyzed_text = TextAnalyzer.new(params[:user_text])
+
+
+    erb :results
+  end
+  
+  
 end
