0b948015feb30a6f62a6904a62b65db4d1b9753d
allison cortez <allizetorc@gmail.com>
2020-01-24 03:10:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,17 +1,20 @@
+
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
   get '/' do
     erb :user_input
   end
   
+  get '/piglatinize' do
+    "Hello World"
+  end
+
   post '/piglatinize' do
     pl = PigLatinizer.new
     @piglatin = pl.piglatinize(params[:user_phrase])
     
-    erb :results
-    
   end
-  
-  
+
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,12 +1,15 @@
 class PigLatinizer
+
   def piglatinize(input_str)
     input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
   end
-  
+
+  private
+
   def consonant?(char)
     !char.match(/[aAeEiIoOuU]/)
   end
-  
+
   def piglatinize_word(word)
     # word starts with vowel
     if !consonant?(word[0])
@@ -23,10 +26,12 @@ class PigLatinizer
     end
     word << "ay"
   end
-  
+
   def piglatinize_sentence(sentence)
     sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
   end
-  
-  
+
+
+
+
 end
